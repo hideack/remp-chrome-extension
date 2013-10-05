be51e96c22e29e51321f3f0f1bf8a02bfc825a30
hideack <hide.terai@gmail.com>
2013-10-05 21:33:14 +0900

    未使用の変数削除

--- src/js/background.js
+++ src/js/background.js
@@ -9,7 +9,6 @@ chrome.tabs.onSelectionChanged.addListener(function(tabId, opt) {
 
     var request = {};
     request.message = "parse";
-    window.itemNum = 0;
     
     chrome.tabs.sendRequest(tabId, request, function(){
        //callback
