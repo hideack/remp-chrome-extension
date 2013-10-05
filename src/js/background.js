f1d73ae0eb402333837c4e69d7b40cebbeaa8af4
hideack <hide.terai@gmail.com>
2013-10-05 13:08:55 +0900

    tab切替時のlistenerを追加し、呼び出し時URLパーサー呼び出し

--- src/js/background.js
+++ src/js/background.js
@@ -4,3 +4,16 @@ chrome.extension.onRequest.addListener(
         sendResponse({});
     }
 );
+
+chrome.tabs.onSelectionChanged.addListener(function(tabId, opt) {
+
+    var request = {};
+    request.message = "parse";
+    window.itemNum = 0;
+    
+    chrome.tabs.sendRequest(tabId, request, function(){
+       //callback
+    });
+
+});
+
