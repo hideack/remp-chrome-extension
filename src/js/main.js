f63bcfd276a167ce08b22c7f7122b8e7d1ada22b
hideack <hide.terai@gmail.com>
2013-10-05 23:25:21 +0900

    テンプレートのDOM ID変更

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,25 @@
 var RempExtension = (function(){
-    var _videos = null;
+    var _videos = null;         // ページ中から見つけられたYouTube動画情報
+    var _pageTitle = '';        // ページタイトル
 
     return {
         init: function(){
             console.log("Initialize (main.js)");
             _videos = chrome.extension.getBackgroundPage().videos;
-            console.log(_videos);
+            _pageTitle = chrome.extension.getBackgroundPage().pageTitle;
         },
-        currentPageProperty: function(){
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                var current = tabs[0];
-                console.log(current);
-                chrome.extension.sendRequest({greeting: "hello"}, function(response) {
-                    console.log(response.farewell);
-                });
-            });
+        render: function(){
+            var template = $('#contents_template')[0].innerText;
+            var compiled = _.template(template);
+            var params = {videos: _videos, title: _pageTitle};
+            console.log(params);
+            $("#contents").html(compiled(params));
         }
     }
 })();
 
 RempExtension.init();
+$("#contents").ready(function(){
+    RempExtension.render();
+});
 
