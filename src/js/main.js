4d3e9244e07c45083dcd90daf2f94979bab371c9
hideack <hide.terai@gmail.com>
2013-10-06 15:28:01 +0900

    REMPへのログイン状態をAPI経由で取得してフラグに設定

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 var RempExtension = (function(){
-    var _videos    = null;         // ページ中から見つけられたYouTube動画情報
-    var _pageTitle = '';        // ページタイトル
+    var _login     = false;  // REMPへのログイン状態
+    var _videos    = null;   // ページ中から見つけられたYouTube動画情報
+    var _pageTitle = '';     // ページタイトル
     var _plugin    = '';     // プラグイン用
     return {
         init: function(){
@@ -12,13 +13,28 @@ var RempExtension = (function(){
                 videoParams = _videos.join('|');
                 _plugin = "<iframe width=250px height=80px src='http://www.remp.jp/plugins/appendlibrary/" + _pageTitle + "/" + videoParams + "'></iframe>";
             }
+
         },
         render: function(){
-            var template = $('#contents_template')[0].innerText;
-            var compiled = _.template(template);
-            var params = {videos: _videos, title: _pageTitle, plugin: _plugin};
-            console.log(params);
-            $("#contents").html(compiled(params));
+            $.getJSON("http://www.remp.jp/api/status/login", function(){
+                _login = true;
+            })
+            .fail(function(){
+                _login = false;
+            })
+            .always(function(){
+                var template = $('#contents_template')[0].innerText;
+                var compiled = _.template(template);
+                var params = {videos: _videos, title: _pageTitle, plugin: _plugin, login: _login};
+                console.log(params);
+                $("#contents").html(compiled(params));
+            });
+        },
+        link: function(url, selected) {
+            chrome.tabs.create({
+                'url': url,
+                'selected': selected
+            });
         }
     }
 })();
