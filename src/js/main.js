a3c22fd55be576a9beb435a86a93b3321e7cc8b7
hideack <hide.terai@gmail.com>
2013-10-06 10:58:46 +0900

    RempExtension:init でiframeパーツ用パラメータを作成

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,21 @@
 var RempExtension = (function(){
     var _videos = null;         // ページ中から見つけられたYouTube動画情報
     var _pageTitle = '';        // ページタイトル
-
+    var _videosString = '';     // ページ中から見つけられたYouTube動画IDを|で連結したもの
     return {
         init: function(){
             console.log("Initialize (main.js)");
             _videos = chrome.extension.getBackgroundPage().videos;
             _pageTitle = chrome.extension.getBackgroundPage().pageTitle;
+
+            if(_videos.length > 0) {
+                _videosString = _videos.join('|');
+            }
         },
         render: function(){
             var template = $('#contents_template')[0].innerText;
             var compiled = _.template(template);
-            var params = {videos: _videos, title: _pageTitle};
+            var params = {videos: _videos, title: _pageTitle, videos_string: _videosString};
             console.log(params);
             $("#contents").html(compiled(params));
         }
