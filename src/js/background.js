8d76698e7a622a9a80c25ed68e3f535fff01afad
hideack <hide.terai@gmail.com>
2013-10-07 09:03:02 +0900

    ページ中の動画数に応じたバッジの処理追加

--- src/js/background.js
+++ src/js/background.js
@@ -3,6 +3,24 @@ chrome.extension.onRequest.addListener(
     {
         window.videos = request.videos;
         window.pageTitle  = request.title;
+
+        // Badge
+        var videoNum = window.videos.length;
+
+        var colorPattern;
+        if (videoNum == 0) {
+            colorPattern = [128, 128, 128, 255];
+        } else {
+            colorPattern = [255, 0, 0, 255];
+        }
+
+        var tabId = sender.tab.id;
+        var text = {"text": (String)(videoNum), "tabId": tabId};
+        var color = {"color": colorPattern, "tabId": tabId};
+
+        chrome.browserAction.setBadgeText(text);
+        chrome.browserAction.setBadgeBackgroundColor(color);
+
         sendResponse({});
     }
 );
@@ -11,10 +29,11 @@ chrome.tabs.onSelectionChanged.addListener(function(tabId, opt) {
 
     var request = {};
     request.message = "parse";
-    
+
     chrome.tabs.sendRequest(tabId, request, function(){
        //callback
     });
 
 });
+
 
