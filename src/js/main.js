e8e806bb8310a1584699ad293678d93aadfa946f
hideack <hide.terai@gmail.com>
2013-10-07 09:29:59 +0900

    REMP plugin用のパラメータだけmain.jsで作る様にした

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,14 @@ var RempExtension = (function(){
     var _login     = false;  // REMPへのログイン状態
     var _videos    = null;   // ページ中から見つけられたYouTube動画情報
     var _pageTitle = '';     // ページタイトル
-    var _plugin    = '';     // プラグイン用
+    var _videoIds  = '';     // ページ中動画のVideo IDを連結した文字列
     return {
         init: function(){
             _videos = chrome.extension.getBackgroundPage().videos;
             _pageTitle = chrome.extension.getBackgroundPage().pageTitle;
 
             if(_videos.length > 0) {
-                videoParams = _videos.join('|');
-                _plugin = "<iframe width=250px height=80px src='http://www.remp.jp/plugins/appendlibrary/" + _pageTitle + "/" + videoParams + "'></iframe>";
+                _videoIds = _videos.join('|');
             }
 
         },
@@ -24,7 +23,7 @@ var RempExtension = (function(){
             .always(function(){
                 $.get("/template/popup_content.html", function(template){
                     var compiled = _.template(template);
-                    var params = {videos: _videos, title: _pageTitle, plugin: _plugin, login: _login};
+                    var params = {videos: _videos, title: _pageTitle, videoIds: _videoIds, login: _login};
                     $("#contents").html(compiled(params));
                 });
             });
